0b44d1162c756fe096c0333638dfcc5fa0ba51c8
Peka <galagania3@gmail.com>
2022-04-06 03:10:28 +0300

    daily changes

--- src/test/java/FirstAutotestForm.java
+++ src/test/java/FirstAutotestForm.java
@@ -2,8 +2,11 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import java.util.concurrent.TimeUnit;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
 
 public class FirstAutotestForm {
 
@@ -13,7 +16,7 @@ public class FirstAutotestForm {
     }
 
     @Test
-    void Test321 () {
+    void Test321 () throws InterruptedException {
         //open test site
         open("https://demoqa.com/automation-practice-form");
 
@@ -23,20 +26,36 @@ public class FirstAutotestForm {
         $("[for=\"gender-radio-3\"]").click();
         //$("#gender-radio-1").click();
         $("#userNumber").setValue("6666666655");
-        $("#dateOfBirthInput").setValue("12 Jan 1989");
-        $("#subjects-label").click();
+        $("#dateOfBirthInput").setValue("12 Jan 1989").pressEnter();
+        //$("#subjects-label").click();
         $("[for=\"hobbies-checkbox-1\"]").click();
         $("[for=\"hobbies-checkbox-2\"]").click();
         $("[for=\"hobbies-checkbox-3\"]").click();
         //$("#hobbies-checkbox-1").click();
         //$("#hobbies-checkbox-2").click();
         //$("#hobbies-checkbox-3").click();
-        $("#currentAddress").setValue("123 street");
-        $("#state").$(".css-1uccc91-singleValue").setValue("Uttar Pradesh");
+        //$("#currentAddress").setValue("123 street");
+        $("#state").click();
+        $("#stateCity-wrapper").$(byText("Uttar Pradesh")).click();
+        $("#city").click();
+        $("#stateCity-wrapper").$(byText("Agra")).click();
+        //$("#react-select-3-input").setValue("Uttar Pradesh").pressEnter();
+        //TimeUnit.SECONDS.sleep(2);
+        //$("#react-select-4-input").setValue("Delhi").pressEnter();
+        //$(".css-1hwfws3").$(".css-1uccc91-singleValue").setValue("Uttar Pradesh");
         //$("#state").setValue("Uttar Pradesh");
-        $("#city").setValue("Delhi");
+        //$("#city").setValue("Delhi");
+        //TimeUnit.SECONDS.sleep(3);
+        $("#submit").click();
+        TimeUnit.SECONDS.sleep(3);
+        //$("#subjects-label").pressEnter();
+        //$("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+
+
+        $(".modal-body") .$(".tbody") .$()
     }
 }
 
 
-//class=" css-1uccc91-singleValue"   css-1uccc91-singleValue//
+//class=" css-1uccc91-singleValue"   css-1uccc91-singleValue    react-select-4-input//
+//$x("//td[text()='Student Name']").parent().shouldHave(text("Ivan Ivanov"));
